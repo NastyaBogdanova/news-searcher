f4f90efb5f68ae2372fddb07f489d42792ec2ab4
Nastya Bogdanova <anastasia.bogdanova95@mail.ru>
2020-09-09 21:27:52 +0300

    add validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import "./pages/index/index.css";
 
-console.log("This is index page!");
+import { SearchInput } from './js/components/SearchInput.js';
+import { NewsCard } from './js/components/NewsCard.js';
+
+const searchForm = new SearchInput(document.forms.search);
+
+searchForm.setValidateListener();
+searchForm.setSumbitListener();
+
---
+++ src/js/components/SearchInput.js
@@ -0,0 +1,26 @@
+export class SearchInput {
+    constructor(form) {
+      this.form = form;
+    }
+  
+    _validate(form) {
+        const errorElem = form.lastElementChild;
+        const errorMassage = "Нужно ввести ключевое слово";
+      if (!form.checkValidity()) {
+        errorElem.textContent = errorMassage;
+      } else {
+        errorElem.textContent = " ";
+      }
+    } // использовать эту функцию перед сабмитом
+
+    setValidateListener() {
+        this.form.addEventListener("input", () => this._validate(this.form));
+    }
+
+    setSumbitListener() {
+        this.form.addEventListener("submit", () => {
+            event.preventDefault();
+            this._validate(this.form);
+        });
+    }
+}
