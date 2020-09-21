1216e4a80f2288a53ab90a33a0c182704117fa71
Nastya Bogdanova <anastasia.bogdanova95@mail.ru>
2020-09-21 23:03:51 +0300

    markup statistic diagram

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -7,6 +7,7 @@ const title = document.querySelector(".info__news-name");
 const dates = document.querySelectorAll(".diagram__date");
 const newsAtWeek = document.querySelector(".info__news-week");
 const newsInTitles = document.querySelector(".info__news-titles");
+const diagramPercents = document.querySelectorAll(".diagram__percent");
 
 const dataStorage = new DataStorage();
 
@@ -28,16 +29,13 @@ function renderNewsInTitles() {
 }
 renderNewsInTitles();
 
-
-let todayDate = new Date();
-
-function setDate() {
+function changeDate() {
     let dateFrom = new Date();
     dateFrom.setDate(dateFrom.getDate() - 7);
     return dateFrom;
   }
   
-  let dateFrom = setDate();
+  let dateFrom = changeDate();
 
   function fixDate(date) {
     const options = { day: "numeric" };
@@ -51,34 +49,42 @@ function setDate() {
     return days[date.getDay()];
   }
 
+  function filerDates(date) {
+    const articles = dataStorage.getItem("news");
+    const datesArr = articles.map(function (item) {
+      return item.publishedAt.substr(0,10);
+    });
+  
+  const dates = datesArr.filter(function (item) {
+    return item === date; 
+  });
+  return dates;
+  }
+
   function markupDates() {
+    const percentArr = [];
+    console.log(dates);
     dates.forEach((item) => {
     dateFrom.setDate(dateFrom.getDate() + 1);
+    console.log(dateFrom);
     dateFrom.toISOString();
-const fixedDate = fixDate(dateFrom);
-        item.textContent = fixedDate + `, ${getWeekDay(dateFrom)}`;
+    const fixedDate = fixDate(dateFrom);
+    item.textContent = fixedDate + `, ${getWeekDay(dateFrom)}`;
+    const filteredArr = filerDates(dateFrom.toISOString().substr(0, 10));
+    percentArr.push(filteredArr.length);
     })
+  return percentArr;
+  } 
+
+  function markupPercent() {
+    const totalResults = dataStorage.getItem("totalResults");
+    const percentArr =  markupDates();
+    for (let i = 0; i < percentArr.length; i++) {
+      const percent = (percentArr[i] * 100) / totalResults;
+      diagramPercents[i].textContent = Math.round(percent);
+      diagramPercents[i].style.width = percent + '%';
+    }
   }
-  markupDates(); //здесь должна возвращаться дата и передаваться в функцию фильтра
-
-function filerDates(date) {
-  const articles = dataStorage.getItem("news");
-  const datesArr = articles.map(function (item) {
-    return item.publishedAt.substr(0,10);
-  });
-  console.log(datesArr);
-
-const dates = datesArr.filter(function (item) {
-  return item === date; // для значений, которые делятся на 2 без остатка возвращаем true, таким образом в новый массив попадут только чётные элементы
-});
-
-console.log(dates);
-
-}
-filerDates(todayDate.toISOString().substr(0,10));
-console.log(todayDate.toISOString().substr(0,10));
-
-
-  //фильтр по дате
-  //для каждоый даты отфильтровать и отрисовать 
+  markupPercent();
+ 
   
